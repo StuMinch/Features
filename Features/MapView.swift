9a719848732c72d116d7719e605de178bbaa1033
Stuart Minchington <stumin@saucelabs.com>
2024-11-04 18:28:48 -0800

    Added a Map with CoreLocation

--- Features/MapView.swift
+++ Features/MapView.swift
@@ -4,13 +4,21 @@
 //
 //  Created by Stuart Minchington on 10/28/24.
 //
-import CoreLocation
 import MapKit
 import SwiftUI
 
 struct MapView: View {
+    let manager = CLLocationManager()
+    @State private var cameraPosition: MapCameraPosition = .userLocation(fallback: .automatic)
     var body: some View {
-        Map()
+        Map(position: $cameraPosition) {
+            UserAnnotation()
+        }.mapControls {
+            MapUserLocationButton()
+        }
+        .onAppear {
+            manager.requestWhenInUseAuthorization()
+        }
     }
 }
 
